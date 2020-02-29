467bbc21cb1df5088ee8bfc07a72ee1661f6d8b4
toumb <ioannis.toumboglou@gmail.com>
2020-02-29 20:53:09 +0200

    First commit

---
+++ src/main/java/com/toumb/employeeappspring/EmployeeAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.toumb.employeeappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAppSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toumb/employeeappspring/EmployeeAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toumb.employeeappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
