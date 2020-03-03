8034afa3cc2eeab2cbfdd43709a32054819e97d0
toumb <ioannis.toumboglou@gmail.com>
2020-03-03 22:17:50 +0200

    Employee repository DAO

---
+++ src/main/java/com/toumb/employeeappspring/dao/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.toumb.employeeappspring.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.toumb.employeeappspring.entity.Employee;
+
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	// Add a method to sort records by last name
+	public List<Employee> findAllByOrderByTitleAsc();
+
+}
