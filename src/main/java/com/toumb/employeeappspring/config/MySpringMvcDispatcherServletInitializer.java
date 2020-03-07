88214557fcd2a3cc895c7f1fa0f5c20af8f82fb4
toumb <ioannis.toumboglou@gmail.com>
2020-03-07 23:47:30 +0200

    Security config

---
+++ src/main/java/com/toumb/employeeappspring/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,28 @@
+package com.toumb.employeeappspring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.toumb.employeeappspring.EmployeeAppSpringApplication;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		
+		return null;
+	}
+	// dispatcher configuration
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class[] {EmployeeAppSpringApplication.class};
+	}
+
+	// servlet-mapping configuration
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[] {"/"};
+	}
+
+}
