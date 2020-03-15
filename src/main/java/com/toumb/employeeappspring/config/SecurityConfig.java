ee7750a56fe35468a43c7e2c5f24871e66b21597
toumb <ioannis.toumboglou@gmail.com>
2020-03-15 23:20:22 +0200

    Custom security implementation

--- src/main/java/com/toumb/employeeappspring/config/SecurityConfig.java
+++ src/main/java/com/toumb/employeeappspring/config/SecurityConfig.java
@@ -1,41 +1,45 @@
-//package com.toumb.employeeappspring.config;
-//
-//import javax.sql.DataSource;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//	@Autowired
-//	private DataSource securityDataSource;
-//	
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		// Use JDBC authentication
-//		auth.jdbcAuthentication().dataSource(securityDataSource);
-//	}
-//
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//
-//		http.authorizeRequests()
-//			.antMatchers("/employees/list").hasRole("EMPLOYEE")
-//			.antMatchers("/employees/**").hasRole("EMPLOYEE")
-//			.anyRequest().authenticated()
-//			.and()
-//			.formLogin()
-//				.loginPage("/login")
-//				.loginProcessingUrl("/authenticateTheUser")
-//				.permitAll()
-//			.and()
-//			.logout().permitAll();
-//	}
-//		
-//}
+package com.toumb.employeeappspring.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource securityDataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Use JDBC authentication
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/employees/**").hasRole("EMPLOYEE")
+			.antMatchers("/security/login*").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/security/login")
+				.defaultSuccessUrl("/employees/list", true)
+				.failureUrl("/security/login-error")
+				.permitAll()
+			.and()
+			.logout()
+			.logoutUrl("/security/logout")
+			.deleteCookies("JSESSIONID");
+	}
+		
+}
--- src/main/java/com/toumb/employeeappspring/controller/LoginController.java
+++ src/main/java/com/toumb/employeeappspring/controller/LoginController.java
@@ -1,23 +1,28 @@
-//package com.toumb.employeeappspring.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/")
-//public class LoginController {
-//
-//	@GetMapping("/login")
-//	public String showLoginPage() {
-//			
-//		return "login";
-//	}
-//	
-//	@GetMapping("/access-denied")
-//	public String showAccessDenied() {
-//			
-//		return "access-denied";
-//	}
-//	
-//}
+package com.toumb.employeeappspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/security")
+public class LoginController {
+
+	@GetMapping("/login")
+	public String login() {
+		
+		return "security/login";
+	}
+	
+	@GetMapping("/login-error")
+	public String loginError() {
+		
+		return "security/login-error";
+	}
+
+	@GetMapping("/logout")
+	public String logout() {
+		
+		return "security/login";
+	}
+}
